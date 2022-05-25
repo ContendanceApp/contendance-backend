85269bde9f6cb9fecf83b57b0ad83903f06ad9cc
ladasattar <ladasattar@gmail.com>
2022-05-25 18:54:41 +0700

    feat: add response relation model

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -27,7 +28,7 @@ class AuthController extends Controller
     {
         $credentials = request(['email', 'password']);
 
-        if (! $token = auth()->attempt($credentials)) {
+        if (!($token = auth()->attempt($credentials))) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
@@ -41,7 +42,10 @@ class AuthController extends Controller
      */
     public function me()
     {
-        return response()->json(auth()->user());
+        $user = User::where('user_id', auth()->user()->user_id)
+            ->with('studyGroup')
+            ->first();
+        return response()->json($user);
     }
 
     /**
@@ -78,7 +82,10 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'expires_in' =>
+                auth()
+                    ->factory()
+                    ->getTTL() * 60,
         ]);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -20,21 +20,14 @@ class User extends Authenticatable implements JWTSubject
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = ['name', 'email', 'password'];
 
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token'];
 
     /**
      * The attributes that should be cast.
@@ -65,5 +58,10 @@ class User extends Authenticatable implements JWTSubject
     public function getJWTCustomClaims()
     {
         return [];
+    }
+
+    public function studyGroup()
+    {
+        return $this->hasOne(StudyGroup::class, 'study_group_id');
     }
 }
