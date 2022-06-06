4ebc32ac8560b1c00bea3cf9e43652f45b67c559
ladasattar <ladasattar@gmail.com>
2022-06-06 09:23:46 +0700

    fix: token expired

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -97,7 +97,7 @@ class AuthController extends Controller
             'expires_in' =>
                 auth()
                     ->factory()
-                    ->getTTL() * 60,
+                    ->getTTL() * 43200,
         ]);
     }
 }
