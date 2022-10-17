40f73257ab4a1e95f7ce3621cdfe5ea7f7226114
ladasattar <ladasattar@gmail.com>
2022-10-17 10:45:30 +0700

    fix: add auto run swagger doc

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ COPY package*.json ./
 
 RUN npm i
 
+RUN npm run swagger-autogen
+
 COPY ./ ./ 
 
 RUN npx prisma migrate reset --force
--- swagger.js
+++ swagger.js
@@ -1,6 +1,16 @@
 const swaggerAutogen = require("swagger-autogen")();
 
+const doc = {
+  info: {
+    title: "Contendance Rest API",
+    version: "3.0.0",
+  },
+  host: "api.contendance.studio",
+  basePath: "/api",
+  schemes: ["https"],
+};
+
 const outputFile = "./swagger_output.json";
 const endpointsFiles = ["./routes/index.js"];
 
-swaggerAutogen(outputFile, endpointsFiles);
+swaggerAutogen(outputFile, endpointsFiles, doc);
