00845b529223c3845fd43422131ce62c15158429
ladasattar <ladasattar@gmail.com>
2022-10-17 09:50:54 +0700

    feat: get logged user information

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -74,6 +74,24 @@ module.exports = {
     }
   },
 
+  me: async (req, res) => {
+    try {
+      const user_id = req.user.user_id;
+      const response = await prisma.users.findFirst({
+        where: {
+          user_id,
+        },
+      });
+      if (!response) {
+        return res.status(404).json({ message: "Not Found!" });
+      }
+
+      res.status(200).json({ message: "Data Retrieved!", data: response });
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  },
+
   getAllUsers: async (req, res) => {
     try {
       const response = await prisma.users.findMany({});
