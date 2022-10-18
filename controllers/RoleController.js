b81eef5d7355f13e6ccc4f03cf119c3ce44769e8
ladasattar <ladasattar@gmail.com>
2022-10-18 16:29:30 +0700

    fix: role response

--- controllers/RoleController.js
+++ controllers/RoleController.js
@@ -6,9 +6,9 @@ module.exports = {
   getRoles: async function (req, res) {
     try {
       const response = await prisma.roles.findMany();
-      res.status(200).json(response);
+      res.status(200).json({ message: "Data Retrieved!", data: response });
     } catch (error) {
-      res.status(500).json({ msg: error.message });
+      res.status(500).send(error.message);
     }
   },
 
@@ -19,9 +19,9 @@ module.exports = {
           role_id: Number(req.params.id),
         },
       });
-      res.status(200).json(response);
+      res.status(200).json({ message: "Data Retrieved!", data: response });
     } catch (error) {
-      res.status(404).json({ msg: error.message });
+      res.status(500).send(error.message);
     }
   },
 
@@ -33,9 +33,9 @@ module.exports = {
           role: role,
         },
       });
-      res.status(201).json(roles);
+      res.status(201).json({ message: "Data Created!", data: roles });
     } catch (error) {
-      res.status(400).json({ msg: error.message });
+      res.status(500).send(error.message);
     }
   },
 
@@ -50,9 +50,9 @@ module.exports = {
           role: role,
         },
       });
-      res.status(201).json(roles);
+      res.status(201).json({ message: "Data Updated!", data: roles });
     } catch (error) {
-      res.status(400).json({ msg: error.message });
+      res.status(500).send(error.message);
     }
   },
 
@@ -63,9 +63,9 @@ module.exports = {
           role_id: Number(req.params.id),
         },
       });
-      res.status(201).json(roles);
+      res.status(200).json({ message: "Data Deleted!" });
     } catch (error) {
-      res.status(400).json({ msg: error.message });
+      res.status(500).send(error.message);
     }
   },
 };
