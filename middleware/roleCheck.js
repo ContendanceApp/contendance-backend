f9c009d098780081c1389203deb99ea67f25086b
ladasattar <ladasattar@gmail.com>
2023-01-03 20:05:55 +0800

    feat: new roles checker

--- middleware/roleCheck.js
+++ middleware/roleCheck.js
@@ -51,4 +51,30 @@ module.exports = {
       }
     });
   },
+
+  adminOrDosen: async (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (token === null)
+      return res.status(401).json({ message: "Unauthorized" });
+
+    jwt.verify(token, process.env.JWT_SECRET_TOKEN, async (err, user) => {
+      try {
+        if (err) return res.status(401).json({ message: "Unauthorized" });
+        const auth = await prisma.users.findFirst({
+          where: { email: user.email },
+        });
+
+        if (!auth) return res.status(401).json({ message: "Unauthorized" });
+        else if (auth.role_id !== 2) {
+          if (auth.role_id !== 3)
+            return res.status(403).json({ message: "Forbidden!" });
+        }
+        next();
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    });
+  },
 };
